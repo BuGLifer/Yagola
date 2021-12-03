8ab80c884a96bd104cd7eca8a0f9c15cd969da3d
swgil007 <swgil007@naver.com>
2021-12-03 10:09:07 +0900

    change class name TestController > HelloController

--- src/main/java/com/buglifer/yagola/web/HelloController.java
+++ src/main/java/com/buglifer/yagola/web/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TestController {
+public class HelloController {
 
     @GetMapping("/test")
     public String hello(TestDTO testDTO) {
