62678083a92995e307630ec2c3a6d97c5f0dfcdd
swgil007 <swgil007@naver.com>
2021-12-06 08:34:05 +0900

    [#39] init thyleaf html layout test

---
+++ src/main/java/com/buglifer/yagola/web/WebController.java
@@ -0,0 +1,15 @@
+package com.buglifer.yagola.web;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@AllArgsConstructor
+@Controller
+public class WebController {
+
+    @GetMapping("/main")
+    public String getMain() {
+        return "html/Main";
+    }
+}
