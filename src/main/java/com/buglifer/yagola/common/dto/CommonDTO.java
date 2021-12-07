7e1d3aa0460802475fb5a001f7a6056bbf617df9
swgil007 <swgil007@naver.com>
2021-12-08 08:37:40 +0900

    [#45] add CommonDTO

---
+++ src/main/java/com/buglifer/yagola/common/dto/CommonDTO.java
@@ -0,0 +1,15 @@
+package com.buglifer.yagola.common.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+public class CommonDTO {
+
+    private long seq;
+    private Date createdTime;
+
+}
