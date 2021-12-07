249ffaf0f0d4f4199dc1eb244f5bede42aacade4
swgil007 <swgil007@naver.com>
2021-12-08 08:51:45 +0900

    [#45] add Restaurant Category Enum

---
+++ src/main/java/com/buglifer/yagola/common/enums/restaurant/Category.java
@@ -0,0 +1,24 @@
+package com.buglifer.yagola.common.enums.restaurant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+@Getter
+public enum Category {
+    CHINA("중식")
+    , KOREA("한식")
+    , JAPAN("일식")
+    , ALONE("1인분")
+    , FRANCHISE("프랜차이즈")
+    , WESTERN("양식")
+    , PIG("족발/보쌈")
+    , NIGHT("야식")
+    , SCHOOL("분식")
+    , CAFE("카페/디저트")
+    , MART("마트/편의점")
+    , ETC("기타");
+    private String name;
+}
