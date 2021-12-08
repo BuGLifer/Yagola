13a3a94ab781b014c2fa4641253ab4edc29378be
swgil007 <swgil007@naver.com>
2021-12-08 19:40:38 +0900

    [#43] add UserOrder Entity

---
+++ src/main/java/com/buglifer/yagola/common/domain/UserOrderEntity.java
@@ -0,0 +1,38 @@
+package com.buglifer.yagola.common.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(callSuper = true, of = "seq")
+@Entity
+public class UserOrderEntity extends CommonEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "UO_SEQ")
+    private long seq;
+
+    @Column(name = "HOST")
+    private boolean host;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "U_SEQ")
+    private UserEntity user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "O_SEQ")
+    private OrderEntity order;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "M_SEQ")
+    private MenuEntity menu;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "RV_SEQ")
+    private ReviewEntity review;
+
+}
