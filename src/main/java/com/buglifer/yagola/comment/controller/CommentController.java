95371cc2f077d0c874d778f75d400db18146ad8b
swgil007 <swgil007@naver.com>
2021-12-09 08:26:00 +0900

    [#31] add /comments/{seq} api

--- src/main/java/com/buglifer/yagola/comment/controller/CommentController.java
+++ src/main/java/com/buglifer/yagola/comment/controller/CommentController.java
@@ -1,13 +1,22 @@
 package com.buglifer.yagola.comment.controller;
 
+import com.buglifer.yagola.comment.dto.CommentDTO;
+import com.buglifer.yagola.comment.service.CommentService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("comments")
 @RestController
 public class CommentController {
+
+    private final CommentService commentService;
+
+    @GetMapping("{seq}")
+    public ResponseEntity<CommentDTO> getCommentBySeq(@PathVariable(name = "seq") long seq) {
+        return ResponseEntity.ok().body(commentService.getCommentDTO(seq));
+    }
 
 }
--- src/main/java/com/buglifer/yagola/comment/dto/CommentDTO.java
+++ src/main/java/com/buglifer/yagola/comment/dto/CommentDTO.java
@@ -1,12 +1,29 @@
 package com.buglifer.yagola.comment.dto;
 
+import com.buglifer.yagola.common.domain.CommentEntity;
+import com.buglifer.yagola.common.domain.UserEntity;
 import com.buglifer.yagola.common.dto.CommonDTO;
+import com.buglifer.yagola.common.dto.PostDTO;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
-@ToString
-public class CommentDTO extends CommonDTO {
+@NoArgsConstructor
+@ToString(callSuper = true)
+public class CommentDTO extends PostDTO {
+
+    private long pSeq;
+
+    public CommentDTO(CommentEntity commentEntity) {
+        setSeq(commentEntity.getSeq());
+        pSeq = commentEntity.getPSeq();
+        setView(commentEntity.isView());
+        setCreatedTime(commentEntity.getCreatedTime());
+        if(commentEntity.getComment() != null) {
+            setComment(commentEntity.getComment());
+        }
+    }
 }
