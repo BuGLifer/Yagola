ea79d3ad1da1b9a2c4ef1177eab9955d3b3d4324
swgil007 <swgil007@naver.com>
2021-12-08 19:36:28 +0900

    [#43] add Review Entity

---
+++ src/main/java/com/buglifer/yagola/common/domain/ReviewEntity.java
@@ -0,0 +1,41 @@
+package com.buglifer.yagola.common.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(callSuper = true, of = "seq")
+@Entity
+public class ReviewEntity extends CommonEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "RV_SEQ")
+    private long seq;
+
+    @Column(name = "COMMENT")
+    private String comment;
+
+    @Column(name = "LIKE")
+    private boolean like;
+
+    @Column(name = "VIEW")
+    private boolean view;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "M_SEQ")
+    private MenuEntity menu;
+
+    public void setMenu(MenuEntity menuEntity) {
+        if(menu != null) {
+            menu.getReviews().remove(this);
+        }
+        menu = menuEntity;
+        if(!menu.getReviews().contains(this)) {
+            menu.getReviews().add(this);
+        }
+    }
+}
