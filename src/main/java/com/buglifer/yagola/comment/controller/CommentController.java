918e51265805f256a43911065288d2104dd18ee7
swgil007 <swgil007@naver.com>
2021-12-10 11:06:51 +0900

    [#31] add view search in comment

--- src/main/java/com/buglifer/yagola/comment/controller/CommentController.java
+++ src/main/java/com/buglifer/yagola/comment/controller/CommentController.java
@@ -1,10 +1,12 @@
 package com.buglifer.yagola.comment.controller;
 
 import com.buglifer.yagola.comment.dto.CommentDTO;
+import com.buglifer.yagola.comment.search.CommentSearch;
 import com.buglifer.yagola.comment.service.CommentService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +17,11 @@ import org.springframework.web.bind.annotation.*;
 public class CommentController {
 
     private final CommentService commentService;
+
+    @GetMapping("")
+    public ResponseEntity<Page<CommentDTO>> getComments(CommentSearch search) {
+        return ResponseEntity.ok().body(commentService.findComments(search));
+    }
 
     @GetMapping("{seq}")
     public ResponseEntity<CommentDTO> getCommentBySeq(@PathVariable(name = "seq") long seq) {
