8e9a2b3928825a6a14efb7d8852a40db0f0b6d91
Jeje <ksa24601@gmail.com>
2021-12-13 23:59:20 +0900

    [#27] user controller 추가

---
+++ src/main/java/com/buglifer/yagola/user/service/UserService.java
@@ -0,0 +1,14 @@
+package com.buglifer.yagola.user.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    //private final UserRepository userRepository;
+
+    //public
+
+}
