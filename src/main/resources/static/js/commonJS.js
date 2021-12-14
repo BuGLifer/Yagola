8f2e841c395c03c75a66033d732e0857886f567c
swgil007 <swgil007@naver.com>
2021-12-14 17:43:27 +0900

    [#52] add callAPI function in common

--- src/main/resources/static/js/commonJS.js
+++ src/main/resources/static/js/commonJS.js
@@ -1,7 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
-    alert("b");
-});
-
-const test = function() {
-    alert("a");
+const callAPI = function(method, url, body) {
+    console.log("[callAPI] START");
+    console.log("[callAPI] Method : " + method);
+    console.log("[callAPI] url : " + url);
+    const xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        if(xhr.status === 200 || xhr.status ===201) {
+            console.log("[callAPI] Response : " + xhr.responseText);
+            console.log("[callAPI] END");
+            return xhr.responseText;
+        }
+        console.log("[callAPI] Error");
+        console.log(xhr.responseText);
+    };
+    xhr.open(method, url)
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(body));
 };
