dca4715d8e8ce9ed1aad1777ae20ebb3ed3b58dc
swgil007 <swgil007@naver.com>
2021-12-15 07:51:45 +0900

    [#51] add Yagola main, gola recruit page link, shape in Header

--- src/main/resources/static/js/golaJS.js
+++ src/main/resources/static/js/golaJS.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", function() {
     const callBackFunc = function(response) {
         alert(response)
     };
-    callAPI('GET', 'http://localhost/comments', '', callBackFunc);
+    //callAPI('GET', 'http://localhost/comments', '', callBackFunc);
 });
