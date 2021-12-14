6102c05b049c1941c6077be24465eafea6be454c
swgil007 <swgil007@naver.com>
2021-12-14 17:48:28 +0900

    test call api

---
+++ src/main/resources/static/js/mainJS.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const callBackFunc = function(response) {
+        alert(response)
+    };
+    callAPI('GET', 'http://localhost/comments', '', callBackFunc);
+});
