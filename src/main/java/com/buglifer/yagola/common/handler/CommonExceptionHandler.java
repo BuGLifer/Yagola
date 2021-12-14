58feb725b2f8aa9069ca93583c70e968b617f022
swgil007 <swgil007@naver.com>
2021-12-14 19:24:20 +0900

    [#49] add Security 403 FORBIDDEN exception handle

--- src/main/java/com/buglifer/yagola/common/handler/CommonExceptionHandler.java
+++ src/main/java/com/buglifer/yagola/common/handler/CommonExceptionHandler.java
@@ -1,23 +1,35 @@
 package com.buglifer.yagola.common.handler;
 
 import com.buglifer.yagola.common.dto.ExceptionResponse;
+import org.springframework.expression.AccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.persistence.EntityNotFoundException;
+import java.nio.file.AccessDeniedException;
 
 @RestControllerAdvice
 public class CommonExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ExceptionResponse> handleEntityNotFoundException(EntityNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ExceptionResponse
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                ExceptionResponse
                 .initException()
                 .httpStatus(HttpStatus.BAD_REQUEST)
-                .runtimeException(e)
+                .exception(e)
                 .build());
     }
 
+    @ExceptionHandler(SecurityException.class)
+    public ResponseEntity<ExceptionResponse> handleSecurityException(SecurityException e) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(
+                ExceptionResponse
+                .initException()
+                .httpStatus(HttpStatus.FORBIDDEN)
+                .exception(e)
+                .build());
+    }
 }
