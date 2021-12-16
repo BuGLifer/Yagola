831531f2f19e0a4f3378af070fa1f4814b9cb31d
swgil007 <swgil007@naver.com>
2021-12-17 07:28:23 +0900

    [#51] add select search option function

--- src/main/resources/static/js/top.js
+++ src/main/resources/static/js/top.js
@@ -1,4 +1,5 @@
 const search = function() {
+    console.log(getSelectedCategory());
     const searchValue = document.getElementById('searchBar').value;
     const orderByTime = document.getElementById('orderByTime').checked;
     const orderByCount = document.getElementById('orderByCount').checked;
@@ -7,5 +8,25 @@ const search = function() {
     let queryString = '?orderByTime=' + orderByTime;
     queryString += '&orderByCount=' + orderByCount;
     queryString += '&search=' + searchValue;
-    location.href = url + queryString;
+    //location.href = url + queryString;
+};
+
+const selectSearchOption = function(e) {
+    if(e.classList.contains('selected')) {
+        e.classList.remove('selected');
+        return;
+    }
+    e.classList.add('selected');
+};
+
+const getSelectedCategory = function() {
+    const selectedCategoryList = document.getElementById('categoryList').getElementsByClassName('selected');
+    let selectedCategoryURL = "";
+    if(selectedCategoryList.length == 0) return selectedCategoryURL;
+    selectedCategoryURL = "category=";
+    for(let index = 0; index < selectedCategoryList.length; index++) {
+        selectedCategoryURL += selectedCategoryList[index].id + "&";
+    }
+    selectedCategoryURL = selectedCategoryURL.slice(0, -1);
+    return selectedCategoryURL;
 };
