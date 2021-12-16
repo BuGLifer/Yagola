aae0eadc00c333f0cf3f979ecc47925166d5370d
swgil007 <swgil007@naver.com>
2021-12-17 07:27:58 +0900

    remove test callAPI when document ready

--- src/main/resources/static/js/gola.js
+++ src/main/resources/static/js/gola.js
@@ -1,8 +1,3 @@
 document.addEventListener("DOMContentLoaded", function() {
     setMilestonePath(' > 골라');
-
-    const call = function(response) {
-        console.log(response);
-    };
-    callAPI("GET", "/comments", null, call);
 });
