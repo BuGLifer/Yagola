81f1425d7c1ae69bfad8b8751b1b1896a2573532
swgil007 <swgil007@naver.com>
2021-12-16 09:33:52 +0900

    [#51] keep developing with search logic

--- src/main/resources/static/js/gola.js
+++ src/main/resources/static/js/gola.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     setMilestonePath(' > 골라');
+
+    const call = function(response) {
+        console.log(response);
+    };
+    callAPI("GET", "/comments", null, call);
 });
