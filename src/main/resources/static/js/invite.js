979affcceae87e3724cd3ab52a8a6ba450b53179
swgil007 <48559894+swgil007@users.noreply.github.com>
2021-12-19 12:17:34 +0900

    [#35] add restaurant, guest, order info area in order card

--- src/main/resources/static/js/invite.js
+++ src/main/resources/static/js/invite.js
@@ -5,13 +5,62 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 const createOrderCard = function(order) {
+    console.log(order);
     let orderCardDiv = document.createElement('div');
     orderCardDiv.classList.add('orderCard');
+    let restaurantInfoDiv = document.createElement('div');
+    restaurantInfoDiv.classList.add('restaurantInfo');
+    let guestInfoDiv = document.createElement('div');
+    guestInfoDiv.classList.add('guestInfo');
+    let orderInfoDiv = document.createElement('div');
+    orderInfoDiv.classList.add('orderInfo');
+    orderInfoDiv.append(restaurantInfoDiv);
+    orderCardDiv.append(orderInfoDiv);
+    orderCardDiv.append(guestInfoDiv);
     return orderCardDiv;
 };
 
 const setOrderCard = function() {
     let orderCardArea = document.getElementById('orderCardArea');
     orderCardArea.classList.add('orderCardArea');
-    orderCardArea.append(createOrderCard(null));
+    const testOrder = {
+        "seq" : 1
+        , "status" : "ONLINE"
+        , "createdTime" : "2021-12-18 13:00:00"
+        , "host" : {
+            "seq" : 1
+            , "nickName" : "길로그"
+        }
+        , "guest" : [
+            {
+                "seq" : 2
+                , "nickName" : "참가자1"
+                , "menu" : {
+                    "seq" : 1
+                    , "name" : "닭가슴살"
+                }
+            }
+            , {
+                "seq" : 3
+                , "nickName" : "참가자3"
+                , "menu" : {
+                    "seq" : 2
+                    , "name" : "고구마"
+                }
+            }
+            , {
+                "seq" : 4
+                , "nickName" : "참가자4"
+                , "menu" : {
+                    "seq" : 3
+                    , "name" : "야채믹스"
+                }
+            }
+        ]
+        , "restaurant" : {
+            "seq" : 1
+            , "name" : "골목식당"
+        }
+    };
+    orderCardArea.append(createOrderCard(testOrder));
 };
