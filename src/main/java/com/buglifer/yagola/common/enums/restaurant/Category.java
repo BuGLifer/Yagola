8c504481c3fd6acc663cceb01acf15fb0d323bc1
swgil007 <swgil007@naver.com>
2021-12-24 07:54:44 +0900

    [#50] replace name in category for matching yogiyo response

--- src/main/java/com/buglifer/yagola/common/enums/restaurant/Category.java
+++ src/main/java/com/buglifer/yagola/common/enums/restaurant/Category.java
@@ -4,22 +4,26 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
 
+import java.util.EnumSet;
+
 @ToString
 @AllArgsConstructor
 @Getter
 public enum Category {
     CHINA("중식")
     , KOREA("한식")
-    , JAPAN("일식")
-    , ALONE("1인분")
+    , JAPAN("일식돈까스")
+    , ALONE("1인분주문")
     , FRANCHISE("프랜차이즈")
-    , WESTERN("양식")
-    , PIG("족발/보쌈")
+    , WESTERN("피자양식")
+    , PIG("족발보쌈")
     , NIGHT("야식")
     , SCHOOL("분식")
-    , CAFE("카페/디저트")
-    , MART("마트/편의점")
+    , CAFE("카페디저트")
+    , MART("편의점")
     , CHICKEN("치킨")
     , ETC("기타");
+
     private String name;
+
 }
